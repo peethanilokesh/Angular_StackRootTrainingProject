669a5273632d2ae08049308d4f61ddf32c6ade9c
bhannugalhotra777 <bhannugalhotra777@gmail.com>
2022-02-24 13:11:38 +0000

    added page not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
+import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path : 'home', component:HomeComponent},
-  {path: '', redirectTo:'/home', pathMatch: 'full'}
+  {path: '', redirectTo:'/home', pathMatch: 'full'},
+  {path:'**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
