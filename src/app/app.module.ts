1af15afca6059f1c54315ab1b7a4f9903c15a842
bhannugalhotra777 <bhannugalhotra777@gmail.com>
2022-02-24 12:51:09 +0000

    deleted previos authenticaation componetn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
-import { AuthneticationComponent } from './Components/authnetication/authnetication.component';
+
 import { AuthenticationComponent } from './Components/authentication/authentication.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthneticationComponent,
     AuthenticationComponent
   ],
   imports: [
