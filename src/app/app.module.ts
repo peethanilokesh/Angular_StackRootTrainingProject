ac117d579f4e32bf8b2e42350be526b093ba4800
sonisanjeev650 <sonisanjeev650@gmail.com>
2022-02-24 13:18:54 +0000

    removed unwanted code.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,29 +6,16 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 
 import { AuthenticationComponent } from './Components/authentication/authentication.component';
-import { PatientsListComponent } from './Components/patients-list/patients-list.component';
 import { HomeComponent } from './Components/home/home.component';
-<<<<<<< HEAD
 import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
-=======
-import { AddPatientComponent } from './Components/patients/add-patient/add-patient.component';
->>>>>>> c8341c6f2e4f019a8640cf9a1f0d1ab59d9cc5dc
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AuthenticationComponent,
-<<<<<<< HEAD
-    PatientsListComponent,
-<<<<<<< HEAD
-PageNotFoundComponent
-=======
-AddPatientComponent
->>>>>>> c8341c6f2e4f019a8640cf9a1f0d1ab59d9cc5dc
-=======
+PageNotFoundComponent,
     HomeComponent
->>>>>>> 6f19c8948d2a0bc15092631148fdcd2c70f7354a
   ],
   imports: [
     BrowserModule,
