02efea8737813380c618df335dd2509a7dc36ff2
bhannugalhotra777 <bhannugalhotra777@gmail.com>
2022-02-25 06:44:25 +0000

    logic for edit patient added

--- src/app/Components/patient/edit-patient/edit-patient.component.ts
+++ src/app/Components/patient/edit-patient/edit-patient.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IEditPatient } from 'src/app/Models/Patient/editPatient';
+import { IPatient } from 'src/app/Models/Patient/patient';
+import { PatientService } from 'src/app/Services/patient.services';
 
 @Component({
   selector: 'app-edit-patient',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditPatientComponent implements OnInit {
 
-  constructor() { }
+  newPatient : IEditPatient = {
+    PatientName: '', 
+    PatientAge: 0,
+    PatientGender: '',
+    Department: '',
+    DoctorName: '',
+    DoctorFee: 0
+  }
+  constructor(private patientService : PatientService,
+    private router: Router) {
+      this.patientService.GetPatientById(patientService.idOfPatientToBeEdited)
+      .subscribe((data: IPatient) =>{
+          this.newPatient.PatientName = data.PatientName;
+          this.newPatient.PatientAge = data.PatientAge;
+          this.newPatient.PatientGender = data.PatientGender;
+          this.newPatient.Department = data.Department;
+          this.newPatient.DoctorName = data.DoctorName;
+          this.newPatient.DoctorFee = data.DoctorFee;
+      });
+     }
 
-  ngOnInit(): void {
+  
+
+  ngOnInit(): void {    
+  }
+
+  onEditPatient(){
+    console.log(this.newPatient);
+
+    this.patientService.EditPatient(this.newPatient)
+    .subscribe((data : any) =>
+    {
+      console.log(data);
+      this.router.navigate(['/patientList'])
+    });
   }
 
 }
