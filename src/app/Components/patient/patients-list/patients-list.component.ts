298af7d0203db44a9c652cfb628e1712e377cb9c
bhannugalhotra777 <bhannugalhotra777@gmail.com>
2022-02-25 06:47:03 +0000

    searching and sorted on list added

--- src/app/Components/patient/patients-list/patients-list.component.ts
+++ src/app/Components/patient/patients-list/patients-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IPatient } from 'src/app/Models/Patient/patient';
+import { PatientService } from 'src/app/Services/patient.services';
 
 @Component({
   selector: 'app-patients-list',
@@ -7,9 +10,102 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientsListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private patientServices: PatientService,
+    private router: Router) { }
+
+    d = new Date(2018, 11, 24, 10, 33, 30, 0);
+
+  patients: IPatient[] = [{
+    PatientId: 1,
+    PatientName: "some", 
+    PatientAge: 20,
+    PatientGender: "some",
+    Department: "some",
+    DoctorName: "some",
+    DoctorFee: 200,
+    RegisterDate: this.d,
+    CreatedBy: "some"
+  },
+  {
+    PatientId: 10,
+    PatientName: "xomething", 
+    PatientAge: 20,
+    PatientGender: "some",
+    Department: "some",
+    DoctorName: "some",
+    DoctorFee: 200,
+    RegisterDate: this.d,
+    CreatedBy: "some"
+  }];
+  searchText: string = "";
+  
+  isPatientDataAvailable: boolean = false;
 
   ngOnInit(): void {
+
+    this.patientServices.GetPatients()
+    .subscribe((data: IPatient[]) =>
+    {
+        this.isPatientDataAvailable = true;
+        console.log(data);
+        this.patients = data;
+    }
+    );
+  }
+
+  onClickEdit(id: number){
+    console.log(id)
+
+    this.patientServices.PassIdToEditComponent(id);
+    
+  }
+
+  onClickDelete(id : number){
+    console.log(id);
+    this.patientServices.PassIdToDeleteComponent(id);
+  }
+
+
+  isIdSortedAsc : boolean = false;
+
+  onClickId(){
+    console.log("clicked")
+
+    if(this.isIdSortedAsc){
+      this.patients.sort((a, b) => {return b.PatientId  - a.PatientId});
+      this.isIdSortedAsc = false;
+    }
+
+    else{
+      this.isIdSortedAsc = true;
+      this.patients.sort((a, b) => {return a.PatientId  - b.PatientId});
+    }
+  }
+
+  isNameSortedAsc : boolean = false;
+  
+  onClickName(){
+    console.log("clicked")
+
+    if(this.isNameSortedAsc){
+      this.patients.sort((a, b) => {
+        if(a.PatientName < b.PatientName)
+        return -1;
+
+        return 1;
+      });
+      this.isNameSortedAsc = false;
+    }
+
+    else{
+      this.isNameSortedAsc = true;
+      this.patients.sort((a, b) => {
+        if(a.PatientName < b.PatientName)
+        return 1;
+
+        return -1;
+      });
+    }
   }
 
 }
